5b0ab8205c082825b8ec1dcf2ffff6760d0b3a52
서성준 <tkflwk123@gmail.com>
2023-01-20 20:06:41 +0900

    링크추가 및 날짜형식 변경

--- src/main/java/com/example/jump/controller/MainController.java
+++ src/main/java/com/example/jump/controller/MainController.java
@@ -14,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -65,7 +66,7 @@ public class MainController {
             BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
             StringBuffer result = new StringBuffer();
             String re = null;
-            while((re = br.readLine()) != null)
+            while ((re = br.readLine()) != null)
                 result.append(re);
 
             jsonObject = XML.toJSONObject(result.toString());
@@ -77,28 +78,56 @@ public class MainController {
                 int count = 0;    // 오류 없이 지날 때마다 count증가
                 try {
                     System.out.println(item);
-                    title = ("{"+quotes+"org"+quotes+":"+quotes+item.get("Title").toString()+quotes+"}"); count++;
+                    title = ("{" + quotes + "org" + quotes + ":" + quotes + item.get("Title").toString() + quotes + "}");
+                    count++;
 
-                    if(!item.get("SubTitle1").equals("")) {
-                        subject = (("[{"+quotes+"org"+quotes+":"+quotes+item.get("SubTitle1")+quotes+"}]"));
-                    }else if(!item.get("SubTitle2").equals("")) {
-                        subject = (("[{"+quotes+"org"+quotes+":"+quotes+item.get("SubTitle2")+quotes+"}]"));
-                    }else if(!item.get("SubTitle3").equals("")) {
-                        subject = (("[{"+quotes+"org"+quotes+":"+quotes+item.get("SubTitle3")+quotes+"}]"));
-                    }else {
-                        subject = (("[{"+quotes+"org"+quotes+":"+quotes+item.get("Title")+quotes+"}]"));
-                    }	count++;
+                    if (!item.get("SubTitle1").equals("")) {
+                        subject = (("[{" + quotes + "org" + quotes + ":" + quotes + item.get("SubTitle1") + quotes + "}]"));
+                    } else if (!item.get("SubTitle2").equals("")) {
+                        subject = (("[{" + quotes + "org" + quotes + ":" + quotes + item.get("SubTitle2") + quotes + "}]"));
+                    } else if (!item.get("SubTitle3").equals("")) {
+                        subject = (("[{" + quotes + "org" + quotes + ":" + quotes + item.get("SubTitle3") + quotes + "}]"));
+                    } else {
+                        subject = (("[{" + quotes + "org" + quotes + ":" + quotes + item.get("Title") + quotes + "}]"));
+                    }
+                    count++;
 
-                    description = (("{"+quotes+"summary"+quotes+":{"+quotes+"org"+quotes+":"+quotes+item.get("DataContents")+quotes+"}")); count++;
-                    publisher = ("{"+quotes+"org"+quotes+":"+quotes+item.get("MinisterCode").toString()+quotes+"}"); count++;
-                    contributors = (("[{"+quotes+"org"+quotes+":"+quotes+item.get("MinisterCode")+quotes+","+quotes+"role"+quotes+":"+quotes+"author"+quotes+"}]")); count++;
-                    date = ("{"+quotes+"modified"+quotes+":"+quotes+item.get("ModifyDate").toString()+","+"available:"+item.get("ApproveDate").toString()+quotes+"}"); count++;
-                    language = ("{"+quotes+"org"+quotes+":"+quotes+"ko"+quotes+"}"); count++;
-                    identifier = ("{"+quotes+"site"+quotes+":"+quotes+item.get("NewsItemId").toString()+","+"view:"+item.get("OriginalUrl").toString()+quotes+"}"); count++;
-                    format = (("{"+quotes+"org"+quotes+":"+quotes+quotes+"}")); count++;
-                    relation = ("{"+quotes+"related"+quotes+":["+quotes+item.get("FileName").toString()+quotes+","+quotes+item.get("FileUrl").toString()+quotes+"]}"); count++;
-                    coverage= (("{"+quotes+"org"+quotes+":"+quotes+quotes+"}")); count++;
-                    right= (("{"+quotes+"org"+quotes+":"+quotes+quotes+"}"));
+                    description = (("{" + quotes + "summary" + quotes + ":{" + quotes + "org" + quotes + ":" + quotes + item.get("DataContents") + quotes + "}"));
+                    count++;
+                    publisher = ("{" + quotes + "org" + quotes + ":" + quotes + item.get("MinisterCode").toString() + quotes + "}");
+                    count++;
+                    contributors = (("[{" + quotes + "org" + quotes + ":" + quotes + item.get("MinisterCode") + quotes + "," + quotes + "role" + quotes + ":" + quotes + "author" + quotes + "}]"));
+                    count++;
+
+                    // 날짜 변환 로직
+                    SimpleDateFormat dfFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");    // 파싱 전 형식
+                    SimpleDateFormat newDtFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"); // 파싱 후 형식
+                    String strDate = item.get("ModifyDate").toString();
+                    String strDate2 = item.get("ApproveDate").toString();
+                    String dateTemp = "";
+                    String dateTemp2 = "";
+
+                    if(!strDate.equals("")){    // ModifyDate가 값이 있으면
+                        Date formatDate = dfFormat.parse(strDate);
+                        dateTemp = newDtFormat.format(formatDate);
+                    }
+                    if(!strDate2.equals("")){   // ApproveDate가 값이 있으면
+                        Date formatDate2 = dfFormat.parse(strDate2);
+                        dateTemp2 = newDtFormat.format(formatDate2);
+                    }
+                    count++;
+                    date = ("{" + quotes + "modified" + quotes + ":" + quotes + dateTemp + "," + quotes + "available" + quotes + ":" + quotes + dateTemp2 + quotes + "}");
+                    language = ("{" + quotes + "org" + quotes + ":" + quotes + "ko" + quotes + "}");
+                    count++;
+                    identifier = ("{" + quotes + "site" + quotes + ":" + quotes + item.get("NewsItemId").toString() + "," + "view:" + item.get("OriginalUrl").toString() + quotes + "}");
+                    count++;
+                    format = (("{" + quotes + "org" + quotes + ":" + quotes + quotes + "}"));
+                    count++;
+                    relation = ("{" + quotes + "related" + quotes + ":[" + quotes + item.get("FileName").toString() + quotes + "," + quotes + item.get("FileUrl").toString() + quotes + "]}");
+                    count++;
+                    coverage = (("{" + quotes + "org" + quotes + ":" + quotes + quotes + "}"));
+                    count++;
+                    right = (("{" + quotes + "org" + quotes + ":" + quotes + quotes + "}"));
 
                 } catch (Exception e) {
                     model.addAttribute("error_name", "ERROR : 증분 데이터 ERROR~!!");
@@ -127,7 +156,7 @@ public class MainController {
             model.addAttribute("error_name", "ERROR : 데이터 수집 ERROR~!!");
             model.addAttribute("error_code", "CODE : EF_R_003");
             model.addAttribute("error_reason", "사유: 정확한 인증키를 입력해주시기 바랍니다.");
-            model.addAttribute("error_key", "요청하신 Key: "+serviceKey);
+            model.addAttribute("error_key", "요청하신 Key: " + serviceKey);
             model.addAttribute("error_page", "요청하신 Page: ");
             e.printStackTrace();
             return "api";
@@ -158,72 +187,72 @@ public class MainController {
         model.addAttribute("format2", "format : ");
         model.addAttribute("relation2", "relation : ");
         model.addAttribute("coverage2", "coverage : ");
-        model.addAttribute("right2", "right : " );
+        model.addAttribute("right2", "right : ");
 
-        Map<String,String> map = new HashMap<>();
-        Map<String,String> map2 = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
+        Map<String, String> map2 = new HashMap<>();
 
-        map.put("rawdata_title",pitches.get(0));
-        map.put("rawdata_subject1",pitches.get(1));
-        map.put("rawdata_subject2",pitches.get(2));
-        map.put("rawdata_subject3",pitches.get(3));
-        map.put("rawdata_subject4",pitches.get(4));
-        map.put("rawdata_description1",pitches.get(5));
-        map.put("rawdata_description2",pitches.get(6));
-        map.put("rawdata_publisher",pitches.get(7));
-        map.put("rawdata_contributors1",pitches.get(8));
-        map.put("rawdata_contributors2",pitches.get(9));
-        map.put("rawdata_date1",pitches.get(10));
-        map.put("rawdata_date2",pitches.get(11));
-        map.put("rawdata_identifier1",pitches.get(12));
-        map.put("rawdata_identifier2",pitches.get(13));
-        map.put("rawdata_identifier3",pitches.get(14));
-        map.put("rawdata_format",pitches.get(15));
-        map.put("rawdata_relation1",pitches.get(16));
-        map.put("rawdata_relation2",pitches.get(17));
-        map.put("rawdata_coverage",pitches.get(18));
-        map.put("rawdata_right",pitches.get(19));
+        map.put("rawdata_title", pitches.get(0));
+        map.put("rawdata_subject1", pitches.get(1));
+        map.put("rawdata_subject2", pitches.get(2));
+        map.put("rawdata_subject3", pitches.get(3));
+        map.put("rawdata_subject4", pitches.get(4));
+        map.put("rawdata_description1", pitches.get(5));
+        map.put("rawdata_description2", pitches.get(6));
+        map.put("rawdata_publisher", pitches.get(7));
+        map.put("rawdata_contributors1", pitches.get(8));
+        map.put("rawdata_contributors2", pitches.get(9));
+        map.put("rawdata_date1", pitches.get(10));
+        map.put("rawdata_date2", pitches.get(11));
+        map.put("rawdata_identifier1", pitches.get(12));
+        map.put("rawdata_identifier2", pitches.get(13));
+        map.put("rawdata_identifier3", pitches.get(14));
+        map.put("rawdata_format", pitches.get(15));
+        map.put("rawdata_relation1", pitches.get(16));
+        map.put("rawdata_relation2", pitches.get(17));
+        map.put("rawdata_coverage", pitches.get(18));
+        map.put("rawdata_right", pitches.get(19));
 
-        for(Map.Entry<String, String> entry : map.entrySet()){  // rawdata1
+        for (Map.Entry<String, String> entry : map.entrySet()) {  // rawdata1
             String key = entry.getKey();    // 키를 얻어옴.
             String value = entry.getValue();    // 값을 얻어옴.
-            model.addAttribute(key,value);
+            model.addAttribute(key, value);
         }
 
-        map2.put("rawdata2_title",pitches.get(0));
-        map2.put("rawdata2_subject1",pitches.get(1));
-        map2.put("rawdata2_subject2",pitches.get(2));
-        map2.put("rawdata2_subject3",pitches.get(3));
-        map2.put("rawdata2_subject4",pitches.get(4));
-        map2.put("rawdata2_description1",pitches.get(5));
-        map2.put("rawdata2_description2",pitches.get(6));
-        map2.put("rawdata2_publisher",pitches.get(7));
-        map2.put("rawdata2_contributors1",pitches.get(8));
-        map2.put("rawdata2_contributors2",pitches.get(9));
-        map2.put("rawdata2_date1",pitches.get(10));
-        map2.put("rawdata2_date2",pitches.get(11));
-        map2.put("rawdata2_identifier1",pitches.get(12));
-        map2.put("rawdata2_identifier2",pitches.get(13));
-        map2.put("rawdata2_identifier3",pitches.get(14));
-        map2.put("rawdata2_format",pitches.get(15));
-        map2.put("rawdata2_relation1",pitches.get(16));
-        map2.put("rawdata2_relation2",pitches.get(17));
-        map2.put("rawdata2_coverage",pitches.get(18));
-        map2.put("rawdata2_right",pitches.get(19));
+        map2.put("rawdata2_title", pitches.get(0));
+        map2.put("rawdata2_subject1", pitches.get(1));
+        map2.put("rawdata2_subject2", pitches.get(2));
+        map2.put("rawdata2_subject3", pitches.get(3));
+        map2.put("rawdata2_subject4", pitches.get(4));
+        map2.put("rawdata2_description1", pitches.get(5));
+        map2.put("rawdata2_description2", pitches.get(6));
+        map2.put("rawdata2_publisher", pitches.get(7));
+        map2.put("rawdata2_contributors1", pitches.get(8));
+        map2.put("rawdata2_contributors2", pitches.get(9));
+        map2.put("rawdata2_date1", pitches.get(10));
+        map2.put("rawdata2_date2", pitches.get(11));
+        map2.put("rawdata2_identifier1", pitches.get(12));
+        map2.put("rawdata2_identifier2", pitches.get(13));
+        map2.put("rawdata2_identifier3", pitches.get(14));
+        map2.put("rawdata2_format", pitches.get(15));
+        map2.put("rawdata2_relation1", pitches.get(16));
+        map2.put("rawdata2_relation2", pitches.get(17));
+        map2.put("rawdata2_coverage", pitches.get(18));
+        map2.put("rawdata2_right", pitches.get(19));
 
-        for(Map.Entry<String, String> entry : map2.entrySet()){ // rawdata2
+        for (Map.Entry<String, String> entry : map2.entrySet()) { // rawdata2
             String key = entry.getKey();    // 키를 얻어옴.
             String value = entry.getValue();    // 값을 얻어옴.
-            model.addAttribute(key,value);
+            model.addAttribute(key, value);
         }
 
-        Map<String,String> colon = new HashMap<>();
-        for(int i=0;i<20;i++){	// 20번 반복해서 들어감
-            colon.put("rawdata_Colon"+i,"");
-            if(!(pitches.get(i).equals(""))){
-                colon.put("rawdata_Colon"+i,":");
+        Map<String, String> colon = new HashMap<>();
+        for (int i = 0; i < 20; i++) {    // 20번 반복해서 들어감
+            colon.put("rawdata_Colon" + i, "");
+            if (!(pitches.get(i).equals(""))) {
+                colon.put("rawdata_Colon" + i, ":");
             }
-            model.addAttribute("rawdata_Colon"+i,colon.get("rawdata_Colon"+i));	// key: rawdata_Colon+ i,   value: "" or ":"
+            model.addAttribute("rawdata_Colon" + i, colon.get("rawdata_Colon" + i));    // key: rawdata_Colon+ i,   value: "" or ":"
         }
 
         model.addAttribute("mapping_1", "[ 매핑전 데이터 예시 ]");
